7c96564977b5042c147bbc15f73c3a1b9864ea09
Matthew Pitkin <matthew.pitkin@ligo.org>
2015-08-12 16:47:17 +0100

    evidence_ul_distribution_plots.py: made better boxplots for the odds ratio distributions

--- scripts/evidence_ul_distribution_plots.py
+++ scripts/evidence_ul_distribution_plots.py
@@ -5,11 +5,33 @@ Script to produce plots from the output of evidence_ul_distribution.py
 """
 
 from matplotlib import pyplot as pl
+from matplotlib.patches import Polygon
 
 import numpy as np
 import json
 import os
 import subprocess as sp
+
+# function for filling in boxplots
+def fillboxes(axis, boxplot, data):
+  numBoxes = len(data)
+  medians = range(numBoxes)
+  for i in range(numBoxes):
+    box = boxplot['boxes'][i]
+    boxX = []
+    boxY = []
+    for j in range(5):
+      boxX.append(box.get_xdata()[j])
+      boxY.append(box.get_ydata()[j])
+    boxCoords = zip(boxX,boxY)
+
+    boxPolygon = Polygon(boxCoords, facecolor='grey')
+    axis.add_patch(boxPolygon)
+
+    med = boxplot['medians'][i]
+    # Finally, overplot the sample averages, with horizontal alignment
+    # in the center of each box
+    axis.plot([np.average(med.get_xdata())], [np.average(data[i])], color='w', marker='*', markeredgecolor='k', ms=8)
 
 # set some plot defaults
 pl.rc('text', usetex=True)
@@ -42,9 +64,17 @@ for n in nlives:
 
 fig, ax = pl.subplots(figsize=(8,5))
 bp = pl.boxplot(datast, whis=[5, 95], notch=0, sym='x', positions=np.log2(nlives)) 
-ax.set_xlims((0., np.log2(nlive[-1]*2.)))
-pl.axhline(y=info['Odds ratios']['studentst'], color='k', linewidth=2)
-pl.xlabel('$\log{}_2(\\textrm(Number of live points)$')
+pl.setp(bp['fliers'], color='blue')
+pl.setp(bp['boxes'], color='black')
+pl.setp(bp['whiskers'], color='black')
+
+# fill in the boxes
+fillboxes(ax, bp, datast)
+
+pl.xticks(np.log2(nlives).tolist(), nlives)
+ax.set_xlim((np.log2(nlives[0]/2.), np.log2(nlives[-1]*2.)))
+pl.axhline(y=info['Odds ratios']['studentst'], color='k', linewidth=2, ls='dashed')
+pl.xlabel('Number of live points')
 pl.ylabel('log(Odds Ratio)')
 outfig = os.path.join(basedir, 'oddsratio_distr_studentst.pdf')
 fig.savefig(outfig)
@@ -53,9 +83,17 @@ pl.close(fig)
 
 fig, ax = pl.subplots(figsize=(8,5))
 bp = pl.boxplot(datagauss, whis=[5, 95], notch=0, sym='x', positions=np.log2(nlives)) 
-ax.set_xlims((0., np.log2(nlive[-1]*2.)))
+pl.setp(bp['fliers'], color='blue')
+pl.setp(bp['boxes'], color='black')
+pl.setp(bp['whiskers'], color='black')
+
+# fill in the boxes
+fillboxes(ax, bp, datagauss)
+
+pl.xticks(np.log2(nlives).tolist(), nlives)
+ax.set_xlim((np.log2(nlives[0]/2.), np.log2(nlives[-1]*2.)))
 pl.axhline(y=info['Odds ratios']['gaussian'], color='k', linewidth=2)
-pl.xlabel('$\log{}_2(\\textrm(Number of live points)$')
+pl.xlabel('Number of live points')
 pl.ylabel('log(Odds Ratio)')
 outfig = os.path.join(basedir, 'oddsratio_distr_gaussian.pdf')
 fig.savefig(outfig)

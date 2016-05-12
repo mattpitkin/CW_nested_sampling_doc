fake.py: fix ul calculation

scripts/fake.py
@@ -190,7 +190,7 @@ print("Evidence ratio = %.12e" % evrat)
 
 ct = cumtrapz(h0pdf, lingrid['h0'])
 ctu, ui = np.unique(ct, return_index=True)
-intf = interp1d(ctu, bins[ui], kind='quadratic')
+intf = interp1d(ctu, lingrid['h0'][ui], kind='quadratic')
 
 jsondic['h0uls']['nested'] = intf(0.95)
 jsondic['evrats']['grid'] = evrat
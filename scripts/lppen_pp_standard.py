db2b9e8d4423dadef13813f9323258c13f974da0
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-02-20 16:19:31 +0000

    A couple of scripts to start with run to generate pp plots from

---
+++ scripts/lppen_pp_standard.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+"""
+Run lalapps_pulsar_parameter_estimation_nested analysis over a range of SNRs for signals with parameters drawn randomly from
+some set prior ranges for the four main GW parameters: h0, phi0, cosiota and psi. Sources will be distributed randomly on the sky.
+NOTE: in reality prior if parameters are drawn from a flat prior, and the recovery prior is also flat, but much broader than
+the drawing distributionm then p-p plot should still work (problems will occur is the prior is not flat).
+
+Everything will be hardcoded in this script.
+
+These will be run as a Condor DAG.
+"""
+
+import os
+import sys
+import numpy as np
+import uuid
+import json
+
+from ConfigParser import ConfigParser
+
+import lalapps.pulsarpputils as pppu
+
+# set of SNRs to use
+#snrs = [0., 2., 4., 6., 8., 10., 12., 14., 16., 18., 20.]
+snrs = [0., 2.]
+
+# run base directory (on RAVEN)
+#basedir = '/home/sismp/projects/testing/pp_standard'
+basedir = '/home/matthew/testing/lalapps_knope_O2/outdir'
+
+logdir = os.path.join(basedir, 'log')
+if not os.path.isdir(logdir):
+  os.makedirs(logdir)
+
+# detectors to use
+dets = ['H1', 'L1']
+
+nsigs = 100 # number of signals per SNR
+
+nlive = 1024 # number of live points
+nruns = 2    # number of parallel runs for each analysis
+
+# create sub file for running run_lppen.py
+subfile = os.path.join(basedir, 'lppen.sub')
+subdata = """universe = vanilla
+executable = /home/sismp2/repositories/CW_nest_sampling_doc/scripts/run_lppen.py
+arguments = " $(macroinifile) "
+getenv = True
+log = %s
+error = %s
+output = %s
+notification = never
+accounting_group = ligo.dev.o1.cw.targeted.bayesian
+queue 1
+""" % (os.path.join(logdir, 'run-$(cluster).log'), os.path.join(logdir,'run-$(cluster).err'), os.path.join(logdir,'run-$(cluster).out'))
+
+fp = open(subfile, 'w')
+fp.write(subdata)
+fp.close()
+
+# create dag file
+dagfile = os.path.join(basedir, 'lppen.dag')
+fp = open(dagfile, 'w')
+
+# set flat prior ranges
+h0range = [0., 1e-20]
+phi0range = [0., 2.*np.pi]
+psirange = [0., np.pi/2.]
+cirange = [-1., 1.]
+
+priorstr = """'H0 uniform {} {}\\nPHI0 uniform {} {}\\nPSI uniform {} {}\\nCOSIOTA uniform {} {}'
+""".format(h0range[0], h0range[1], phi0range[0], phi0range[1], psirange[0], psirange[1], cirange[0], cirange[1])
+
+# set fixed h0 value (that will be scale to SNR)
+h0fixed = 1e-24
+
+lppenexec = '/home/sismp2/lscsoft/.virtualenv/lalapps_knope_O2/bin/lalapps_pulsar_parameter_estimation_nested'
+n2pexec = '/home/sismp2/lscsoft/.virtualenv/lalapps_knope_O2/bin/lalapps_nest2pos'
+
+datasigma = [1e-22, 1e-22] # data standard deviation
+
+for snr in snrs:
+  # generate RA and dec values uniformly over the sky
+  ras = 2.*np.pi*np.random.rand(nsigs)
+  decs = -(np.pi/2.) + np.arccos(2.*np.random.rand(nsigs) - 1.)
+
+  snrdir = os.path.join(basedir, 'snr%.1f' % snr)
+  if not os.path.isdir(snrdir):
+    os.makedirs(snrdir)
+
+  for i in range(nsigs):
+    cprun = ConfigParser() # set config parser to output .ini configuration file
+
+    # set executables configuration
+    cprun.add_section('executables')
+    cprun.set('executables', 'lppen', lppenexec)
+    cprun.set('executables', 'n2p', n2pexec)
+
+    # set run information
+    outdir = os.path.join(snrdir, '%04d' % i)
+    if not os.path.isdir(outdir):
+      os.makedirs(outdir)
+    cprun.add_section('run')
+    cprun.set('run', 'outdir', outdir)
+    cprun.set('run', 'outname', 'nest_%04d' % i)
+    cprun.set('run', 'detectors', json.dumps(dets))
+
+    # set RA and DEC
+    h, m, s = pppu.rad_to_hms(ras[i])
+    rastr = pppu.coord_to_string(h, m, s)
+    d, m, s = pppu.rad_to_dms(decs[i])
+    decstr = pppu.coord_to_string(d, m, s)
+
+    cprun.set('run', 'hetparams', json.dumps({'RAJ': rastr, 'DECJ': decstr}))
+
+    # set simulated data parameters
+    cprun.add_section('data')
+    cprun.set('data', 'sigma', json.dumps(datasigma))
+    cprun.set('data', 'start', json.dumps([900000000, 900000000]))
+    cprun.set('data', 'step', json.dumps([60, 60]))
+    cprun.set('data', 'length', json.dumps([1440, 1440]))
+    
+    # set nested sampling parameters
+    cprun.add_section('nestedsampling')
+    cprun.set('nestedsampling', 'nlive', str(nlive))
+    cprun.set('nestedsampling', 'priordata', priorstr)
+    
+    # set injection parameters
+    if snr == 0.:
+      thish0 = 0.
+    else:
+      thish0 = h0fixed
+    thisphi0 = phi0range[0] + np.diff(phi0range)[0]*np.random.rand() # phi0 value
+    thispsi = psirange[0] + np.diff(psirange)[0]*np.random.rand()    # psi value
+    thisci = cirange[0] + np.diff(cirange)[0]*np.random.rand()       # cos(iota) value
+    cprun.add_section('injection')
+    cprun.set('injection', 'snr', str(snr))
+    cprun.set('injection', 'injparams', json.dumps([{'RAJ': rastr, 'DECJ': decstr, 'H0': thish0, 'PHI0': thisphi0, 'PSI': thispsi, 'COSIOTA': thisci}]))
+    cprun.set('injection', 'coherent', 'True')
+    
+    # write out configuration file
+    runconfig = os.path.join(outdir, 'config.ini')
+    fc = open(runconfig, 'w')
+    cprun.write(fc)
+    fc.close()
+
+    # write out to dag file
+    uippen = uuid.uuid4().hex
+    
+    # write out ppen job
+    dagstr = 'JOB {} {}\nRETRY {} 0\nVARS {} macroinifile=\"{}\"\n'.format(uippen, subfile, uippen, uippen, runconfig)
+    fp.write(dagstr)
+
+# close dag file
+fp.close()
---
+++ scripts/run_lppen.py
@@ -0,0 +1,493 @@
+#!/usr/bin/env python
+
+"""
+Short script to run lalapps_pulsar_parameter_estimation_nested based on inputs given in a configuration file.
+The outputs will be par
+"""
+
+from __future__ import print_function, division
+
+import os
+import sys
+import h5py
+import numpy as np
+import argparse
+import json
+import uuid
+import ast
+from scipy import stats
+import subprocess as sp
+from ConfigParser import ConfigParser
+from copy import copy
+
+import lalapps.pulsarpputils as pppu
+from lalapps.pulsarpputils import pulsar_nest_to_posterior as pn2p
+from lalapps.pulsarpputils import upper_limit_greedy as ulg
+
+
+def get_with_default(config, section, name, default):
+  if config.has_option(section, name):
+    return config.get(section, name)
+  else:
+    return default
+
+
+def find_exec_file(filename):
+  """
+  Search through the PATH environment for the first instance of the executable file "filename"
+  """
+
+  if os.path.isfile(filename) and os.access(filename, os.X_OK):
+    return filename # already have the executable file
+
+  # search through PATH environment variable
+  for d in os.environ['PATH'].split(os.pathsep):
+    filecheck = os.path.join(d, filename)
+    if os.path.isfile(filecheck) and os.access(filecheck, os.X_OK):
+      return filecheck
+
+  return None
+
+
+def credible_interval(samples, paramval=None):
+  # for injections, where we have parameter values (paramval) get the corresponding smallest credible
+  # region that contains the parameter value)
+  samples.sort()
+
+  cival = None
+  if paramval != None:
+    cifound = False
+    # loop over different credible intervals until finding the one that contains the injection
+    for cit in range(1, 101):
+      l, h, cr = ci_loop(samples, cit)
+      if paramval >= l and paramval <= h:
+        cifound = True
+        cival = cit
+        break
+    if not cifound:
+      cival = 100
+
+  return cival
+
+
+def ci_loop(sortedsamples, ci):
+  lowbound = sortedsamples[0]
+  highbound = sortedsamples[-1]
+  cregion = highbound - lowbound
+  lsam = len(sortedsamples)
+  cllen = int(lsam*float(ci)/100.)
+  for j in range(lsam-cllen):
+    if sortedsamples[j+cllen] - sortedsamples[j] < cregion:
+      lowbound = sortedsamples[j]
+      highbound = sortedsamples[j+cllen]
+      cregion = highbound - lowbound
+
+  return lowbound, highbound, cregion
+
+
+def get_snr(pfile):
+  # get SNR from file in pdir
+  snr = 0.
+  snrscale = 0. # SNR scale factor from input amplitude (in lppen code)
+  fp = open(pfile, 'r')
+  lines = [line.strip() for line in fp.readlines()]
+
+  if '# Recovered SNR' not in lines:
+    print("Error... no recovered SNRs are given in the SNR file '%s'." % snrfile, file=sys.stderr)
+    sys.exit(1)
+
+  # just return the final (either coherent or single detector-single frequency value of the SNR)
+  linevals = lines[-1].split()
+  snr += float(linevals[-1])
+  
+  # get scale factor (second to last value) if given on a four value line
+  if len(linevals) == 4:
+    snrscale = float(linevals[-2])
+
+  return snr, snrscale
+
+
+parser = argparse.ArgumentParser( )
+parser.add_argument("inifile", help="The configuration (.ini) file")
+parser.add_argument("-r", "--dont-remove", dest="drm", action='store_true', default=False, help="Set to NOT remove files (will be removed by default)")
+
+# parse input options
+opts = parser.parse_args()
+
+inifile = opts.inifile
+if not os.path.isfile(inifile):
+  print("Error... configuration .ini file does not exist", file=sys.stderr)
+  sys.exit(1)
+
+# open and parse config file
+cp = ConfigParser()
+try:
+  cp.read(inifile)
+except:
+  print("Error... cannot parse configuration file '%s'" % inifile, file=sys.stderr)
+  sys.exit(1)
+
+# get information from the configuration file
+
+# get executables
+ppen = get_with_default(cp, 'executables', 'lppen', 'lalapps_pulsar_parameter_estimation_nested')
+ppen = find_exec_file(ppen)
+if ppen is None:
+  print("Error... cannot find 'lalapps_pulsar_parameter_estimation_nested' executable", file=sys.stderr)
+  sys.exit(1)
+
+n2p = get_with_default(cp, 'executables', 'n2p', 'lalapps_nest2pos')
+n2p = find_exec_file(n2p)
+if n2p is None:
+  print("Error... cannot find 'lalapps_nest2pos' executable", file=sys.stderr)
+  sys.exit(1)
+
+# get run information
+outdir = get_with_default(cp, 'run', 'outdir', None)
+if outdir is None:
+  print("Error... an output directory is required", file=sys.stderr)
+  sys.exit(1)
+
+# check if the directory exists
+if not os.path.isdir(outdir):
+  try:
+    os.makedirs(outdir)
+  except:
+    print("Error... could not make output directory '%s'" % outdir, file=sys.stderr)
+    sys.exit(1)
+
+outname = get_with_default(cp, 'run', 'outname', 'nest'+uuid.uuid4().hex) # default to random string
+
+# get number of nested sample runs
+nruns = ast.literal_eval(get_with_default(cp, 'run', 'nruns', 1))
+if nruns < 1:
+  print("Warning... number of nested sample runs is less than 1, default to 1 run", file=sys.stderr)
+  nruns = 1
+
+# get the detectors to use
+dets = ast.literal_eval(get_with_default(cp, 'run', 'detectors', "['H1']"))
+idets = copy(dets)
+ndets = len(dets)
+if len(dets) > 1:
+  dets.append(','.join(dets))
+
+# output files
+outnest = []
+outpost = []
+for j in range(len(dets)):
+  outnestdet = []
+  for i in range(nruns):
+    outnestdet.append(os.path.join(outdir, outname+'_%s_%04d.hdf' % (dets[j].replace(',', ''), i))) # nested sample files
+  outnest.append(outnestdet)
+  outpost.append(os.path.join(outdir, outname+'_%s_post.hdf' % dets[j].replace(',', ''))) # posterior sample file
+
+# get the par file if exists, otherwise create it
+parfe = False
+parfile = get_with_default(cp, 'run', 'parfile', os.path.join(outdir, 'pulsar.par'))
+defaultpar = """'PSRJ J0000+0000\\n\
+RAJ {RAJ}\\n\
+DECJ {DECJ}\\n\
+F0 {F0}\\n\
+F1 {F1}\\n\
+PEPOCH 54660'"""
+
+if not os.path.isfile(parfile):
+  parparams = "{'RAJ': '00:00:00.0', 'DECJ': '00:00:00.0', 'F0': 100, 'F1': -1e-9}"
+  hetpars = ast.literal_eval(get_with_default(cp, 'run', 'hetparams', parparams)) 
+  
+  if not isinstance(hetpars, dict):
+    print("Error... heterodyne parameters must be a dictionary" % outdir, file=sys.stderr)
+    sys.exit(1)
+  else:
+    pp = ast.literal_eval(parparams)
+    for key in pp.keys():
+      if key not in hetpars:
+        hetpars[key] = pp[key]  
+
+  pardata = ast.literal_eval(get_with_default(cp, 'run', 'pardata', defaultpar.format(**hetpars)))
+  fp = open(parfile, 'w')
+  fp.write(pardata)
+  fp.close()
+else:
+  parfe = True
+
+# read in parameters from par file for later
+hetpars = pppu.psr_par(parfile).__dict__
+
+# get simulated data parameters (for each detector specified), or pre-made data files
+datasigma = ast.literal_eval(get_with_default(cp, 'data', 'sigma', '[1e-22]'))     # noise standard deviation
+datastart = ast.literal_eval(get_with_default(cp, 'data', 'start', '[900000000]')) # start GPS time
+datatstep = ast.literal_eval(get_with_default(cp, 'data', 'step', '[60]'))         # data time step
+datalength = ast.literal_eval(get_with_default(cp, 'data', 'length', '[1440]'))      # number of data points
+datafiles = ast.literal_eval(get_with_default(cp, 'data', 'files', "['%s']" % os.path.join(outdir, 'data_{}.txt.gz'))) # pre-made data files, or names of files to be used
+
+# check if data files exist
+dfe = False
+if len(datafiles) == ndets:
+  if np.array([os.path.isfile(datafiles[i]) for i in range(ndets)]).all():
+    dfe = True # files exist
+
+if not dfe:
+  if len(datafiles) == 1 and ndets > 1:
+    df = datafiles[0]
+    datafiles = [df for i in range(ndets)] 
+
+if not np.all(np.array([len(datasigma), len(datasigma), len(datatstep), len(datalength)]) == ndets):
+  print("Error... simulated data parameter numbers must match the number of detectors.", file=sys.stderr)
+  sys.exit(1)
+
+# create the data (if files don't already exist)
+datafilesdict = {}
+injectoutputs = {}    # files output from each individual detector
+injjoint = []
+for j, d in enumerate(idets):
+  datafilesdict[d] = datafiles[j].format(d)
+
+  if not dfe:
+    gpstimes = np.arange(datastart[j], datastart[j]+(datalength[j]-1.)*datatstep[j], datatstep[j])
+    dlen = len(gpstimes)
+    data = datasigma[j]*np.random.randn(dlen, 2)
+
+    # append times and data together
+    tad = np.vstack((gpstimes, data.T)).T
+    np.savetxt(datafilesdict[d], tad, fmt='%.6f %.7e %.7e', delimiter='\t')
+    injectoutputs[d] = os.path.join(outdir, 'injection_data.txt')
+    injjoint.append(injectoutputs[d]+'_%s_2.0' % d) # append output format of actual files
+  else:
+    datafilesdict[d] = datafiles[j]
+
+if dfe:
+  indfile = ','.join(datafilesdict.values())
+else:
+  indfile = ','.join(injjoint)
+
+if len(dets) > 1:
+  datafilesdict[dets[-1]] = indfile # add all data sets for joint analysis
+
+# get nested sampling parameters
+nlive = get_with_default(cp, 'nestedsampling', 'nlive', 1024)
+
+# get proposal ratios
+uniformprop = get_with_default(cp, 'nestedsampling', 'uniformprop', 1)
+walkprop = get_with_default(cp, 'nestedsampling', 'walkprop', 3)
+stretchprop = get_with_default(cp, 'nestedsampling', 'stretchprop', 0)
+
+# get prior file if exists, otherwise create prior
+priorfile = get_with_default(cp, 'nestedsampling', 'priorfile', os.path.join(outdir, 'prior.txt'))
+defaultprior = """'H0 uniform 0.0 1e-20\\n\
+PHI0 uniform 0.0 {}\\n\
+PSI uniform 0.0 {}\\n\
+COSIOTA uniform -1.0 1.0\\n'"""
+priorfe = False
+if not os.path.isfile(priorfile):
+  # check if specified prior file exists, and if not create it
+  priordata = ast.literal_eval(get_with_default(cp, 'nestedsampling', 'priordata', defaultprior.format(np.pi, np.pi/2.)))
+  fp = open(priorfile, 'w')
+  fp.write(priordata)
+  fp.close()
+else:
+  priorfe = True
+
+# get the variable parameters from the prior file
+varpars = []
+fp = open(priorfile, 'r')
+for line in fp.readlines():
+  if line[0] == '#' or len(line) == 0:
+    continue
+  else:
+    varpars.append(line.split()[0].strip())
+
+# simulated injection information
+injsnr = ast.literal_eval(get_with_default(cp, 'injection', 'snr', 0.0)) # injection SNR (scale injection to this SNR)
+injpar = ast.literal_eval(get_with_default(cp, 'injection', 'parfile', "['%s']" % os.path.join(outdir, 'inj_{}_pulsar.par'))) # injection par file for each detector (can be different if wanting incoherent signals)
+defaultinjpar = """'PSRJ J0000+0000\\n\
+RAJ {RAJ}\\n\
+DECJ {DECJ}\\n\
+F0 {F0}\\n\
+F1 {F1}\\n\
+PEPOCH 54660\\n\
+H0 {H0}\\n\
+COSIOTA {COSIOTA}\\n\
+PSI {PSI}\\n\
+PHI0 {PHI0}\\n'"""
+defaultinjparams = "[{'RAJ': '00:00:00.0', 'DECJ': '00:00:00.0', 'F0': 100, 'F1': -1e-9, 'H0': 1e-24, 'COSIOTA': 0.0, 'PSI': 0.5, 'PHI0': 1.4}]"
+injparfe = [False for i in range(ndets)]
+
+# get injection values
+if len(injpar) == 1: # use the same injection file for all detectors
+  injpar = [injpar[0] for i in range(ndets)]
+
+ipe = False
+if len(injpar) == ndets:
+  if np.array([os.path.isfile(datafiles[i]) for i in range(ndets)]).all():
+    ipe = True # files exist
+
+if not ipe:
+  dip = ast.literal_eval(defaultinjparams)[0]
+  injpardet = ast.literal_eval(get_with_default(cp, 'injection', 'injparams', defaultinjparams)) 
+  
+  if len(injpardet) == 1:
+    injpardet = [injpardet[0] for i in range(ndets)]
+
+  for i in range(ndets):
+    if not isinstance(injpardet[i], dict):
+      print("Error... injection parameters must be a dictionary" % outdir, file=sys.stderr)
+      sys.exit(1)
+    else:
+      pp = injpardet[i]
+      for key in pp.keys():
+        if key not in hetpars:
+          injpardet[i][key] = pp[key]  
+  
+  injpardata = ast.literal_eval(get_with_default(cp, 'injection', 'injdata', "["+defaultinjpar.format(**dip)+"]"))
+  if len(injpardata) == 1:
+    injpardata = [injpardata[0] for i in range(ndets)]
+
+  for i in range(ndets):
+    fp = open(injpar[i].format(dets[i]), 'w')
+    fp.write(injpardata[i].format(**injpardet[i]))
+    fp.close()
+
+injcoh = ast.literal_eval(get_with_default(cp, 'injection', 'coherent', 'True')) # check whether injection is coherent between detectors
+
+ppencodecall = "{} --detectors {} --input-files {} --prior-file {} --par-file {} --uniformprop {} --ensembleWalk {} --ensembleStretch {} --outfile {} --Nlive {} --Nmcmcinitial 0 --tolerance 0.1 {}"
+
+n2pcodecall = "{} -p {} {}"
+
+Zs = []        # signal evidences
+Ns = []        # noise evidences
+uls = []       # 95% h0 upper limits
+Zerrs = []     # estimated errors on the evidence
+recSNRs = []   # recovered SNR
+scaleSNRs = {} # scale factors from input SNR to output
+
+cis = {}
+ainjpars = {}
+for j in range(len(dets)):
+  ci = {}
+  
+  extraargs = ""
+  if j < ndets and not dfe: # doing single detector analyses
+    extraargs += "--inject-file {} --inject-output {} --scale-snr {}".format(injpar[j].format(dets[j]), injectoutputs[dets[j]], injsnr)
+
+  snrs = []
+
+  for i in range(nruns):
+    # run lalapps_pulsar_parameter_estimation_nested
+    print(ppencodecall.format(ppen, dets[j], datafilesdict[dets[j]], priorfile, parfile, uniformprop, walkprop, stretchprop, outnest[j][i], nlive, extraargs), file=sys.stdout)
+    p = sp.Popen(ppencodecall.format(ppen, dets[j], datafilesdict[dets[j]], priorfile, parfile, uniformprop, walkprop, stretchprop, outnest[j][i], nlive, extraargs), stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+    out, err = p.communicate()
+    print(err, file=sys.stderr)
+    
+    # get SNRs
+    snrfile = os.path.splitext(outnest[j][i])[0]+'_SNR'
+    snr, snrscale = get_snr(snrfile)
+    snrs.append(snr)
+    
+    if i == 0 and j < ndets:
+      scaleSNRs[dets[j]] = snrscale
+
+  recSNRs.append(np.mean(snrs))
+
+  # run lalapps_nest2pos
+  p = sp.Popen(n2pcodecall.format(n2p, outpost[j], ' '.join(outnest[j])), stdout=sp.PIPE, stderr=sp.PIPE, shell=True)
+  out, err = p.communicate()
+  print(err, file=sys.stderr)
+
+  try:
+    post, Z, N = pn2p(outpost[j])
+    Zs.append(Z)
+    Ns.append(N)
+  except:
+    print("Error... could not extract samples from posterior file '%s'" % outpost[j], file=sys.stderr)
+    sys.exit(1)
+
+  # get 95% upper limit for H0 samples
+  try:
+    uls.append(ulg(post['H0'].samples))
+  except:
+    print("Error... could not get 'H0' upper limit from posterior samples", file=sys.stderr)
+    sys.exit(1)
+    
+  # get error on the Z value
+  hdf = h5py.File(outpost[j], 'r')
+  a = hdf['lalinference']['lalinference_nest']
+  info = a.attrs['information_nats']
+  nlive = a.attrs['number_live_points']
+  Zerrs.append(np.sqrt(info/nlive))
+  
+  # injection parameters
+  if j < ndets:
+    ainjpars[dets[j]] = pppu.psr_par(injpar[j].format(dets[j])).__dict__
+  
+  # get smallest credible interval containing the injection (for p-p plots) - only do this for coherent injections
+  if injcoh:
+    # get heterodyne parameters from par file and injection parameters from par file
+    thetpars = []
+    tinjpars = []
+    for vp in varpars:
+      vpa = vp
+      if vp == 'RAJ' or vp == 'DECJ':
+        vpa = vp.strip('J') + '_RAD'
+
+      tinjpars.append(ainjpars[dets[0]][vpa]) # get from first detector as all values should be the same for coherent injections
+    
+      # credible interval
+      ci[vp] = credible_interval(post[vp].samples[:,0], ainjpars[dets[0]][vpa])
+
+    cis[dets[j]] = ci
+
+# output dictionary
+outdic = {}
+outdic['Detectors'] = dets
+outdic['Injected SNR'] = injsnr
+outdic['Recovered SNR'] = recSNRs
+outdic['SNR scale factors'] = scaleSNRs # factors by which the amplitude had to be scale to give the injected SNR value
+outdic['Signal evidence'] = Zs
+outdic['Noise evidence'] = Ns
+outdic['Injection credible intervals'] = cis
+outdic['Heterodyne parameters'] = hetpars
+
+# output injection parameters
+if len(ainjpars) > 0:
+  outdic['Injection parameters'] = ainjpars
+
+outfile = get_with_default(cp, 'output', 'outfile', os.path.join(outdir, 'stats.json'))
+fp = open(outfile, 'w')
+json.dump(outdic, fp, indent=2)
+fp.close()
+
+# clean up files if required
+if not opts.drm:
+  try:
+    # remove nested sample files
+    for f in outnest:
+      for ff in f:
+        os.remove(ff)
+  
+    # remove posterior samples
+    for f in outpost:
+      os.remove(ff)
+    
+    # remove data files (if not previously existing files)
+    if not dfe:
+      for d in idets:
+        os.remove(datafilesdict[d])
+        os.remove(injectoutputs[d]+'*')
+
+    # remove par file
+    if not parfe:
+      os.remove(parfile)
+  
+    # remove prior file
+    if not priorfe:
+      os.remove(priorfile)
+
+    # remove injection files
+    if not ipe:
+      for f in injpar:
+        os.remove(f)
+  except:
+    print("Warning... could not remove various files.", file=sys.stderr)
+
+sys.exit(0)

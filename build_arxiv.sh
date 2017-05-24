0b73a960484c5ee07c7498e0f59b1fa2dcd2b779
Matthew Pitkin <matthew.pitkin@ligo.org>
2017-05-24 23:11:49 +0100

    tar file with arXiv submission - some minor differences

--- build_arxiv.sh
+++ build_arxiv.sh
@@ -1,4 +1,4 @@
-figs=`find figures -name *.eps`
+figs=`find figures -name '*.pdf'`
 caps=`find figures -name caption.tex`
 
-tar -cvzf arXiv.tar.gz abstract.tex acknowledgements.tex appendix0*.tex header.tex emulateapj.cls main.tex main.bbl posttitle.tex section0*.tex $figs $caps
+tar -cvzf arXiv.tar.gz abstract.tex acknowledgements.tex appendix0*.tex emulateapj.cls main.tex main.bbl posttitle.tex section0*.tex $figs $caps
